dfe651f7ecda408e550012c6d1f0e95f1e284b3d
Sanjay Dhakad <sanjay.dk17@gmail.com>
2022-09-19 14:23:00 +0530

    initial setup for nx caching

---
+++ main-app/apps/my-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'my-app';
+  myTitle = "Sanjay  2020";
+}
